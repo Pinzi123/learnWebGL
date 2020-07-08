fbf4ec205aa9d84ff589bfdf1b79730805b32a1e
yaping.wang <yaping.wang@fuwo.com>
2020-07-08 19:09:44 +0800

    save

--- script/shader/edge.js
+++ script/shader/edge.js
@@ -1,4 +1,8 @@
-//边缘检测
+//边缘检测（对屏幕图像进行边缘检测，实现描边的效果）
+/**
+ * 卷积操作指的是使用一个卷积核对一张图像中的每个像素进行一系列操作。
+ * 卷积操作的神奇之处在于选择的卷积核。
+ */
 
 const vsSource = `
     precision mediump float;
@@ -31,8 +35,11 @@ const vsSource = `
     }
   `;
 
-// Fragment shader program
-
+/**
+ * 用于边缘检测的卷积核，叫做边缘检测算子，就是Gx和Gy。
+ * 如果相邻像素之间存在明显的颜色、亮度、纹理等属性，我们就任务他们之间存在一条边界。
+ * 这种相邻元素的差值用梯度来表示，不难想象，边缘处的梯度绝对值会比较大。
+ */
 const fsSource = `
     precision mediump float;
 
@@ -40,7 +47,8 @@ const fsSource = `
     varying mediump vec2 vTextureUV[9];
     uniform sampler2D uSampler;
 
-    const float edgeOnly = 1.0;
+    
+    const float edgeOnly = 1.0;//调节这个加上原图的颜色
     const vec4 edgeColor = vec4(0.0,0.0,0.0,1.0);
     const vec4 BColor = vec4(1.0,1.0,1.0,1.0);
 
@@ -79,18 +87,16 @@ const fsSource = `
         float edgeX = 0.0;
         float edgeY = 0.0;
         float texColor;
-        vec4 color = vec4(1.0,1.0,1.0,1.0);
 
         for (int i=0; i<9; i++){
-            color = texture2D(uSampler, vTextureUV[i]);
-            texColor = 0.2125*color.r + 0.7154*color.g + 0.0721*color.b;
+            texColor = lum(texture2D(uSampler, vTextureUV[i]));
             edgeX = edgeX + texColor * Gx[i];
             edgeY = edgeY + texColor * Gy[i];
         }
        float edge = 1.0 - abs(edgeX) - abs(edgeY);
 
        gl_FragColor = mix(
-        mix(edgeColor, texture2D(uSampler, vTextureUV[4]),edge),
+        mix(edgeColor, texture2D(uSampler, vTextureUV[4]), edge),
         mix(edgeColor, BColor, edge),
         edgeOnly);
       
